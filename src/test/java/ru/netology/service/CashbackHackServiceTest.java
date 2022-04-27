8e360b5ca2c632447adf9ccbfdc820804d3ea002
Сергей Душенбиев <agnium@mail.ru>
2022-04-27 16:21:44 +0300

    Tests modified

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,29 +2,25 @@ package ru.netology.service;
 
 public class CashbackHackServiceTest {
 
+    CashbackHackService service = new CashbackHackService ();
+
     @org.testng.annotations.Test
     public void shouldReturnOneThousand() {
-        CashbackHackService service = new CashbackHackService ();
-        int amount = 0;
-        int expected = service.remain(amount);
+        int expected = service.remain(0);
         int actual = 1000;
         org.testng.Assert.assertEquals(actual, expected);
         }
 
     @org.testng.annotations.Test
     public void shouldReturnNineHundredAndNinetyNineA() {
-        CashbackHackService service = new CashbackHackService ();
-        int amount = 1;
-        int expected = service.remain(amount);
+        int expected = service.remain(1);
         int actual = 999;
         org.testng.Assert.assertEquals(actual, expected);
     }
 
     @org.testng.annotations.Test
     public void shouldReturnNineHundredAndNinetyNineB() {
-        CashbackHackService service = new CashbackHackService ();
-        int amount = 1001;
-        int expected = service.remain(amount);
+        int expected = service.remain(1001);
         int actual = 999;
         org.testng.Assert.assertEquals(actual, expected);
     }
@@ -32,18 +28,14 @@ public class CashbackHackServiceTest {
 
     @org.testng.annotations.Test
     public void shouldReturnOne() {
-        CashbackHackService service = new CashbackHackService ();
-        int amount = 999;
-        int expected = service.remain(amount);
+        int expected = service.remain(999);
         int actual = 1;
         org.testng.Assert.assertEquals(actual, expected);
     }
 
     @org.testng.annotations.Test
     public void shouldReturnZero() {
-        CashbackHackService service = new CashbackHackService ();
-        int amount = 1000;
-        int expected = service.remain(amount);
+        int expected = service.remain(1000);
         int actual = 0;
         org.testng.Assert.assertEquals(actual, expected);
     }
