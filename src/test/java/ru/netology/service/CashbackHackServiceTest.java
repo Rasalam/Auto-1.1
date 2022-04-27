bff723a6e7cb986955e249c9ff1591ae3d066f64
Сергей Душенбиев <agnium@mail.ru>
2022-04-27 18:10:58 +0300

    Tests and settings modified for JUnit4 under Junit Jupiter

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,45 +1,84 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.Assert;
+
 
 public class CashbackHackServiceTest {
 
-    CashbackHackService service = new CashbackHackService ();
+    CashbackHackService service = new CashbackHackService();
 
     @Test
-    public void shouldReturnOneThousand() {
+    public void shouldReturnOneThousandJUnitJupiter() {
         int expected = service.remain(0);
         int actual = 1000;
-        org.junit.Assert.assertEquals(actual, expected);
+        Assertions.assertEquals(actual, expected);
     }
 
     @Test
-    public void shouldReturnNineHundredAndNinetyNineA() {
+    public void shouldReturnNineHundredAndNinetyNineJUnitJupiterA() {
         int expected = service.remain(1);
         int actual = 999;
-        org.junit.Assert.assertEquals(actual, expected);
+        Assertions.assertEquals(actual, expected);
     }
 
     @Test
-    public void shouldReturnNineHundredAndNinetyNineB() {
+    public void shouldReturnNineHundredAndNinetyNineJUnitJupiterB() {
         int expected = service.remain(1001);
         int actual = 999;
-        org.junit.Assert.assertEquals(actual, expected);
+        Assertions.assertEquals(actual, expected);
     }
 
 
     @Test
-    public void shouldReturnOne() {
+    public void shouldReturnOneJUnitJupiter() {
         int expected = service.remain(999);
         int actual = 1;
-        org.junit.Assert.assertEquals(actual, expected);
+        Assertions.assertEquals(actual, expected);
     }
 
     @Test
-    public void shouldReturnZero() {
+    public void shouldReturnZeroJUnitJupiter() {
         int expected = service.remain(1000);
         int actual = 0;
-        org.junit.Assert.assertEquals(actual, expected);
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnOneThousandJUnit4() {
+        int expected = service.remain(0);
+        int actual = 1000;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnNineHundredAndNinetyNineJUnit4A() {
+        int expected = service.remain(1);
+        int actual = 999;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnNineHundredAndNinetyNineJUnit4B() {
+        int expected = service.remain(1001);
+        int actual = 999;
+        Assert.assertEquals(actual, expected);
+    }
+
+
+    @Test
+    public void shouldReturnOneJUnit4() {
+        int expected = service.remain(999);
+        int actual = 1;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnZeroJUnit4() {
+        int expected = service.remain(1000);
+        int actual = 0;
+        Assert.assertEquals(actual, expected);
     }
 
 }
