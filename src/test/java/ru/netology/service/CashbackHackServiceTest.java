5d60275715acd5944ad66ffde408016ec7fd1cdf
Сергей Душенбиев <agnium@mail.ru>
2022-04-27 16:18:59 +0300

    Added Tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,11 +2,44 @@ package ru.netology.service;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class CashbackHackServiceTest {
 
+    CashbackHackService service = new CashbackHackService ();
+
     @Test
-    public void remain() {
+    public void shouldReturnOneThousand() {
+        int expected = service.remain(0);
+        int actual = 1000;
+        org.junit.Assert.assertEquals(actual, expected);
     }
+
+    @Test
+    public void shouldReturnNineHundredAndNinetyNineA() {
+        int expected = service.remain(1);
+        int actual = 999;
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnNineHundredAndNinetyNineB() {
+        int expected = service.remain(1001);
+        int actual = 999;
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+
+    @Test
+    public void shouldReturnOne() {
+        int expected = service.remain(999);
+        int actual = 1;
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnZero() {
+        int expected = service.remain(1000);
+        int actual = 0;
+        org.junit.Assert.assertEquals(actual, expected);
+    }
+
 }
